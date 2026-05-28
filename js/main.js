24a3bd9eeedbe90ea87b0f236f9184f4e46a1896
marfc <75501666+marfc@users.noreply.github.com>
2026-05-28 16:41:13 +0200

    ft: added menu images and food

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 console.log("Carta QR cargada 🍽️");
+
+console.log("Carta QR cargada 🍽️");
+
+const modal = document.getElementById("imageModal");
+const modalImage = document.getElementById("modalImage");
+const closeModal = document.querySelector(".close-modal");
+
+const clickableItems = document.querySelectorAll(".clickable");
+
+clickableItems.forEach(item => {
+
+  item.addEventListener("click", () => {
+
+    const image = item.querySelector(".dish-image");
+
+    modalImage.src = image.src;
+
+    modal.classList.add("active");
+
+  });
+
+});
+
+closeModal.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+modal.addEventListener("click", (e) => {
+
+  if (e.target === modal) {
+    modal.classList.remove("active");
+  }
+
+});
